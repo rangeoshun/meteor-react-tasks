5b6115262df7fc8c791934f1f8869af842537910
Abel Varga <rangeoshun@gmail.com>
2017-09-28 20:08:43 +0200

    Use self published ssr for style injection

--- client/main.jsx
+++ client/main.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Meteor } from "meteor/meteor"
 import { render } from "react-dom"
 
-import { createRouter, logger } from "meteor/ssrwpo:ssr"
+import { createRouter, logger } from "meteor/rangeoshun:ssr"
 
 import MainClient from "/imports/ui/components/MainClient"
 import appReducers from "/imports/reducers"
--- server/main.js
+++ server/main.js
@@ -1,12 +1,12 @@
 import "/imports/api/server/publications"
 import "/imports/api/server/methods"
 
-import { createRouter, logger } from "meteor/ssrwpo:ssr"
+import { createRouter, logger } from "meteor/rangeoshun:ssr"
 
 import MainServer from "/imports/ui/components/MainServer"
 import appReducers from "/imports/reducers"
 
 createRouter({
-	MainApp: MainServer,
-	appReducers
+  MainApp: MainServer,
+  appReducers
 })
